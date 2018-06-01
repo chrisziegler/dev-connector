470e443aca54009d737f71b247082c3dee17849c
chrisziegler <chrisrziegler@gmail.com>
2018-06-01 10:54:51 -0700

    Finished ProfileCreds mapping and formatting Education and Experience

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 import PropTypes from 'prop-types';
 
 export class ProfileCreds extends Component {
@@ -7,64 +8,56 @@ export class ProfileCreds extends Component {
   };
 
   render() {
+    const { education, experience } = this.props.profile;
     return (
       <div className="row">
         <div className="col-md-6">
           <h3 className="text-center text-info">Experience</h3>
           <ul className="list-group">
-            <li className="list-group-item">
-              <h4>Microsoft</h4>
-              <p>Oct 2011 - Current</p>
-              <p>
-                <strong>Position:</strong> Senior Developer
-              </p>
-              <p>
-                <strong>Description:</strong> Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Unde doloribus
-                dicta enim excepturi laborum voluptatem nam provident
-                quisquam facere. Quae?
-              </p>
-            </li>
-            <li className="list-group-item">
-              <h4>Sun Microsystems</h4>
-              <p>Oct 2004 - Nov 2011</p>
-              <p>
-                <strong>Position: </strong> Systems Admin
-              </p>
-              <p>
-                <span>
-                  <strong>Location: </strong> Miami, FL
-                </span>
-                <strong>Description: </strong> Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Unde doloribus
-                dicta enim excepturi laborum voluptatem nam provident
-                quisquam facere. Quae?
-              </p>
-            </li>
+            {experience.map(exp => (
+              <li key={exp._id} className="list-group-item">
+                <h4>{exp.company}</h4>
+                <p>
+                  <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+                  {exp.to ? (
+                    <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+                  ) : (
+                    ' Now'
+                  )}
+                </p>
+                <p>
+                  <strong>Position:</strong> {exp.title}
+                </p>
+                <p>
+                  <strong>Description:</strong> {exp.description}
+                </p>
+              </li>
+            ))}
           </ul>
         </div>
 
         <div className="col-md-6">
           <h3 className="text-center text-info">Education</h3>
           <ul className="list-group">
-            <li className="list-group-item">
-              <h4>Univeresity Of Washington</h4>
-              <p>Sep 1993 - June 1999</p>
-              <p>
-                <strong>Degree: </strong>Masters
-              </p>
-              <p>
-                <strong>Field Of Study: </strong>Computer Science
-              </p>
-              <p>
-                <span>
-                  <strong>Description:</strong> Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit. Unde doloribus
-                  dicta enim excepturi laborum voluptatem nam
-                  provident quisquam facere. Quae?
-                </span>
-              </p>
-            </li>
+            {education.map(edu => (
+              <li key={edu._id} className="list-group-item">
+                <h4>{edu.school}</h4>
+                <p>Sep 1993 - June 1999</p>
+                <p>
+                  <strong>Degree: </strong>
+                  {edu.degree}
+                </p>
+                <p>
+                  <strong>Field Of Study: </strong>
+                  {edu.fieldofstudy}
+                </p>
+                <p>
+                  <span>
+                    <strong>Description:</strong> {edu.description}
+                  </span>
+                </p>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
