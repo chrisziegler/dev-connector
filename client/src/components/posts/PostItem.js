a47ed03abdb2103fd7305a547ab108ee4f621f36
chrisziegler <chrisrziegler@gmail.com>
2018-06-12 10:31:53 -0700

    Mocked-in PostItem from html template

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { Link } from 'react-router-dom';
 
 export class PostItem extends Component {
-  static propTypes = { post: PropTypes.array.isRequired };
+  static propTypes = {
+    post: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+  };
 
   render() {
-    const { name, avatar, text } = this.props.post;
+    const {
+      post: { text, name, avatar },
+      auth
+    } = this.props;
     return (
-      <div>
-        <h4>{name}</h4>
-        <p>{text}</p>
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
+            <a href="profile.html">
+              <img
+                className="rounded-circle d-none d-md-block"
+                src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+                alt=""
+              />
+            </a>
+            <br />
+            <p className="text-center">John Doe</p>
+          </div>
+          <div className="col-md-10">
+            <p className="lead">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Sint possimus corporis sunt necessitatibus! Minus
+              nesciunt soluta suscipit nobis. Amet accusamus
+              distinctio cupiditate blanditiis dolor? Illo perferendis
+              eveniet cum cupiditate aliquam?
+            </p>
+            <button type="button" className="btn btn-light mr-1">
+              <i className="text-info fas fa-thumbs-up" />
+              <span className="badge badge-light">4</span>
+            </button>
+            <button type="button" className="btn btn-light mr-1">
+              <i className="text-secondary fas fa-thumbs-down" />
+            </button>
+            <a href="post.html" className="btn btn-info mr-1">
+              Comments
+            </a>
+            {/* <button type="button" className="btn btn-danger mr-1">
+                    <i className="fas fa-times" />
+                  </button> */}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default PostItem;
+// we need to check if post is by current user to allow delete - need auth state
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PostItem);
